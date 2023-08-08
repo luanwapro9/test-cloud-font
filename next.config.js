ef518d6c239358b6fe17927eebd835d5d250c26a
LUAN\luand <minh.luan@vand.vn>
2023-08-08 11:21:03 +0700

    test

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'out',
+   images: {
+    loader: 'akamai',
+    path: '',
+  },
+  future: {
+    excludeDefaultMomentLocales: true
+  },
+
 }
 
 module.exports = nextConfig
